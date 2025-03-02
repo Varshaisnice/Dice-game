e74daf95ca8e81283ccce84f28e776eac05d464e
Varshaisnice <153986098+Varshaisnice@users.noreply.github.com>
2025-03-02 22:56:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1=Math.random();
 randomNumber1=Math.floor(randomNumber1*6)+1;
 var randomNumber2=Math.random();
 randomNumber2=Math.floor(randomNumber2*6)+1;
-
+document.querySelector("h1").innerHTML="Refresh Me";
 
 if(randomNumber1===1){document.querySelector(".dice .img1").setAttribute("src","dice1.png");}
 if(randomNumber1===2){document.querySelector(".dice .img1").setAttribute("src","dice2.png");}
